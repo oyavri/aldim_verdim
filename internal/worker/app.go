78b8d242f365e2f2f73abaf5837f31a212aa15b6
oyavri <oyavri@gmail.com>
2025-07-23 22:22:47 +0300

    Implement worker logic using consumer, service and repository

--- internal/worker/app.go
+++ internal/worker/app.go
@@ -7,7 +7,9 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/oyavri/aldim_verdim/pkg/db"
+	"github.com/oyavri/aldim_verdim/pkg/entity"
 )
 
 func Run() {
@@ -27,18 +29,43 @@ func Run() {
 	// Since I am using single broker, I will wrap the only broker into a slice
 	kafkaConsumer := NewKafkaConsumer([]string{cfg.Broker}, cfg.BrokerTopic)
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	repository := NewWalletRepository(dbPool)
+	service := NewEventService(repository)
 
 	// Graceful shutdown
-	go func() {
+	go func(dbPool *pgxpool.Pool, kc *KafkaConsumer, cancelFunc context.CancelFunc) {
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 		<-signalChan
 		signal.Stop(signalChan)
 
 		log.Println("Consumer is closing")
-		kafkaConsumer.Close()
+		kc.Close()
 
 		log.Println("Database connection is closing")
 		dbPool.Close()
-	}()
+
+		cancelFunc()
+	}(dbPool, kafkaConsumer, cancel)
+
+	for {
+		e, err := kafkaConsumer.Consume(ctx)
+		if err != nil {
+			if ctx.Err() != nil {
+				log.Println("Context cancelled, stopping worker")
+				return
+			}
+
+			log.Printf("Error consuming event: %v", err)
+			continue
+			// Retry mechanism might be helpful here, if it is possible to save the event
+		}
+
+		go func(event entity.Event) {
+			err := service.HandleEvent(ctx, event)
+			if err != nil {
+				log.Printf("Failed to handle event %v with the error: %v", event, err)
+			}
+		}(e)
+	}
 }
