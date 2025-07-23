a05c56d0a78cd199f48b0cb61f59afad7b2e79d0
oyavri <oyavri@gmail.com>
2025-07-23 21:27:53 +0300

    Initialize db scripts, create tables for transactions, insert a user for testing purposes

---
+++ migrations/01_init_db.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS wallet;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS user_wallets;
+
+CREATE TABLE wallet (
+    id VARCHAR PRIMARY KEY,
+    currency CHAR(3) NOT NULL,
+    amount NUMERIC(16, 3) NOT NULL,
+);
+
+CREATE TABLE user (
+    id VARCHAR PRIMARY KEY
+);
+
+CREATE TABLE user_wallets (
+    userId VARCHAR NOT NULL,
+    walletId VARCHAR NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (walletId) REFERENCES wallet(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
