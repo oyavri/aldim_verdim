7162ebeb3b4c8179dbf6351202bac6e9f7ed8a57
oyavri <oyavri@gmail.com>
2025-09-05 16:15:34 +0300

    Fix SQL query errors on creating wallets with existing currencies (due to unique constraint on currency, it was not possible to create another wallet with same currency) and prevent negative balance on a wallet balance

--- internal/worker/sql/decrease_balance.sql
+++ internal/worker/sql/decrease_balance.sql
@@ -1,5 +1,6 @@
 UPDATE wallet_balance 
 SET amount = wallet_balance.amount - $3
-WHERE $3 > 0
+WHERE   amount >= $3
+    AND $3 > 0
     AND walletId = $1
     AND currency = $2;
--- internal/worker/sql/increase_balance.sql
+++ internal/worker/sql/increase_balance.sql
@@ -1,7 +1,5 @@
 INSERT INTO wallet_balance (walletId, currency, amount) 
 VALUES ($1, $2, $3)
-ON CONFLICT (currency) DO UPDATE
+ON CONFLICT (walletId, currency) DO UPDATE
 SET amount = wallet_balance.amount + EXCLUDED.amount
     WHERE EXCLUDED.amount > 0
-        AND wallet_balance.walletId = $1
-        AND wallet_balance.currency = $2;
