843292cf9bbdec6ceb04aa9a95cfe116feb8f8d0
oyavri <oyavri@gmail.com>
2025-09-05 16:17:25 +0300

    Increase test data population

--- migrations/02_init_data.sql
+++ migrations/02_init_data.sql
@@ -1,3 +1,21 @@
 INSERT INTO wallet_user (id) VALUES ('01HPMV114ZE7Z54M6XV8H4EEMB');
 INSERT INTO wallet (id) VALUES ('01HPMV01XPAXCG242W7SZWD0S5');
 INSERT INTO user_wallets (walletId, userId) VALUES ('01HPMV01XPAXCG242W7SZWD0S5', '01HPMV114ZE7Z54M6XV8H4EEMB');
+
+INSERT INTO wallet_user (id) VALUES ('user_a');
+INSERT INTO wallet_user (id) VALUES ('user_b');
+INSERT INTO wallet_user (id) VALUES ('user_c');
+INSERT INTO wallet_user (id) VALUES ('user_d');
+INSERT INTO wallet_user (id) VALUES ('user_e');
+
+INSERT INTO wallet (id) VALUES ('wallet_a');
+INSERT INTO wallet (id) VALUES ('wallet_b');
+INSERT INTO wallet (id) VALUES ('wallet_c');
+INSERT INTO wallet (id) VALUES ('wallet_d');
+INSERT INTO wallet (id) VALUES ('wallet_e');
+
+INSERT INTO user_wallets (walletId, userId) VALUES ('wallet_a', 'user_a');
+INSERT INTO user_wallets (walletId, userId) VALUES ('wallet_b', 'user_b');
+INSERT INTO user_wallets (walletId, userId) VALUES ('wallet_c', 'user_c');
+INSERT INTO user_wallets (walletId, userId) VALUES ('wallet_d', 'user_d');
+INSERT INTO user_wallets (walletId, userId) VALUES ('wallet_e', 'user_e');
